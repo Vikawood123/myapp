8912e1e297ec4aa51dc4bcd38b675212efb1cfdc
Viktoria :) <VolkovaVika123455@gmail.com>
2026-07-01 15:50:29 +0300

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
