814de21e63ffdb1b1fb2660fd2698939962c5acf
Viktoria :) <VolkovaVika123455@gmail.com>
2026-07-01 15:19:03 +0300

    Add files via upload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,5 @@
 <?php
 
-// ПРИНУДИТЕЛЬНЫЕ ПЕРЕМЕННЫЕ ДЛЯ RENDER
-putenv('APP_ENV=production');
-putenv('APP_URL=https://myapp-cutg.onrender.com');
-putenv('DB_CONNECTION=sqlite');
-putenv('DB_DATABASE=/var/www/html/database/database.sqlite');
-
 /*
 |--------------------------------------------------------------------------
 | Create The Application
